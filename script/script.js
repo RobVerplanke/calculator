576040bb25f7bfe20340d60bed6d57d088883b22
RobusV <rverplanke@gmail.com>
2023-02-04 20:53:58 +0100

    make calculation functions and a 'operate' function

--- script/script.js
+++ script/script.js
@@ -2,12 +2,33 @@ const container = document.getElementById("container");
 const display = document.getElementById("display");
 const buttonHolder = document.getElementById("button-holder");
 
-    
-    // Create 16 buttons
-    for (a = 0; a < 16; a++){
-        let square = document.createElement('button');
-        square.innerText = a;
-        buttonHolder.appendChild(square).className = "button";
-    }
+// Functions
 
-    
+    // add
+function addNumbers(n1, n2) {
+    return n1 + n2;
+}
+
+    // subtract
+function substNumbers(n1, n2) {
+    return n1 - n2;
+}
+
+    // multiply
+function multiNumbers(n1, n2) {
+    return n1 * n2;
+}
+
+    // divide
+function divideNumbers(n1, n2) {
+    return n1 / n2;
+}
+
+    // operate
+function operate(op, n1, n2){
+    if (op == "+") { return addNumbers(n1, n2);
+    }else if (op == "-") { return substNumbers(n1, n2);
+    }else if(op == "*") { return multiNumbers(n1, n2);
+    }else if(op == "/") { return divideNumbers(n1, n2);
+    }
+}
