32108696e32ddccfd04b5a277282e99e0eeca6ee
RobusV <rverplanke@gmail.com>
2023-02-04 21:02:31 +0100

    clean up code

--- script/script.js
+++ script/script.js
@@ -2,33 +2,20 @@ const container = document.getElementById("container");
 const display = document.getElementById("display");
 const buttonHolder = document.getElementById("button-holder");
 
-// Functions
+// Function add
+function addNumbers(n1, n2) {return n1 + n2;}
+// Function subtract
+function substNumbers(n1, n2) {return n1 - n2;}
+// Function multiply
+function multiNumbers(n1, n2) {return n1 * n2;}
+// Function divide
+function divideNumbers(n1, n2) {return n1 / n2;}
 
-    // add
-function addNumbers(n1, n2) {
-    return n1 + n2;
-}
-
-    // subtract
-function substNumbers(n1, n2) {
-    return n1 - n2;
-}
-
-    // multiply
-function multiNumbers(n1, n2) {
-    return n1 * n2;
-}
-
-    // divide
-function divideNumbers(n1, n2) {
-    return n1 / n2;
-}
-
-    // operate
+// Function operate
 function operate(op, n1, n2){
-    if (op == "+") { return addNumbers(n1, n2);
+          if (op == "+") { return addNumbers(n1, n2);
     }else if (op == "-") { return substNumbers(n1, n2);
-    }else if(op == "*") { return multiNumbers(n1, n2);
-    }else if(op == "/") { return divideNumbers(n1, n2);
-    }
+    }else if (op == "*") { return multiNumbers(n1, n2);
+    }else if (op == "/") { return divideNumbers(n1, n2);
+    }else return "Invalid operator";
 }
